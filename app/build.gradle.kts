d005b72c3a0d7e03facd06eb6cd20f709dd58197
NichitaFerelin <68856530+NichitaFerelin@users.noreply.github.com>
2022-12-22 16:26:00 +0200

    Code Static Analysis init (#5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin)
+    id(libs.plugins.android.application.get().pluginId)
+    id(libs.plugins.kotlin.get().pluginId)
 }
 
 android {
