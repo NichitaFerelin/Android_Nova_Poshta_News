8d841da1a9601f49d1e083431021c0400a607338
NichitaFerelin <68856530+NichitaFerelin@users.noreply.github.com>
2023-01-11 13:23:37 +0200

    News Content feature exclude (#16)

--- app/src/main/java/com/ferelin/novaposhtanews/data/remote/api/newsuacontent/NewsUaContentResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.ferelin.novaposhtanews.data.remote.api.newsuacontent
-
-@JvmInline
-value class NewsUaContentResponse(
-    val textBlocks: List<String>,
-)
--- app/src/test/java/com/ferelin/novaposhtanews/tests/data/remote/api/newsmd/NewsMdResponseTest.kt
+++ app/src/test/java/com/ferelin/novaposhtanews/tests/data/remote/api/newsmd/NewsMdResponseTest.kt
@@ -1,9 +1,6 @@
 package com.ferelin.novaposhtanews.tests.data.remote.api.newsmd
 
-import com.ferelin.novaposhtanews.data.remote.api.newsmd.NewsMdApiContent
 import com.ferelin.novaposhtanews.data.remote.api.newsmd.NewsMdApiItem
-import com.ferelin.novaposhtanews.data.remote.api.newsmd.NewsMdApiMedia
-import com.ferelin.novaposhtanews.data.remote.api.newsmd.NewsMdApiSummary
 import com.ferelin.novaposhtanews.data.remote.api.newsmd.NewsMdApiTitle
 import kotlin.test.assertTrue
 import org.junit.Test
@@ -13,27 +10,21 @@ internal class NewsMdResponseTest {
     @Test
     fun `NewsMdApiItem properties get & set`() {
         val expectedId = 0
+        val expectedUrlPath = "url path"
         val expectedTitle = NewsMdApiTitle("ro", "ru")
-        val expectedSummary = NewsMdApiSummary("ro", "ru")
-        val expectedContent = NewsMdApiContent("ro", "ru")
         val expectedCreatedAt = "created at"
-        val expectedMedia = listOf(NewsMdApiMedia("url"))
 
         val actual = NewsMdApiItem(
             expectedId,
+            expectedUrlPath,
             expectedTitle,
-            expectedSummary,
-            expectedContent,
             expectedCreatedAt,
-            expectedMedia,
         )
 
         assertTrue { actual.id == expectedId }
+        assertTrue { actual.urlPath == expectedUrlPath }
         assertTrue { actual.title == expectedTitle }
-        assertTrue { actual.summary == expectedSummary }
-        assertTrue { actual.content == expectedContent }
         assertTrue { actual.createdAt == expectedCreatedAt }
-        assertTrue { actual.media == expectedMedia }
     }
 
     @Test
@@ -44,32 +35,5 @@ internal class NewsMdResponseTest {
 
         assertTrue { actual.ro == expectedRo }
         assertTrue { actual.ru == expectedRu }
-    }
-
-    @Test
-    fun `NewsMdApiSummary properties get & set`() {
-        val expectedRo = "ro summary"
-        val expectedRu = "ru summary"
-        val actual = NewsMdApiSummary(expectedRo, expectedRu)
-
-        assertTrue { actual.ro == expectedRo }
-        assertTrue { actual.ru == expectedRu }
-    }
-
-    @Test
-    fun `NewsMdApiContent properties get & set`() {
-        val expectedRo = "ro content"
-        val expectedRu = "ru content"
-        val actual = NewsMdApiContent(expectedRo, expectedRu)
-
-        assertTrue { actual.ro == expectedRo }
-        assertTrue { actual.ru == expectedRu }
-    }
-
-    @Test
-    fun `NewsMdApiMedia properties get & set`() {
-        val expected = "url 1"
-        val actual = NewsMdApiMedia(expected)
-        assertTrue { actual.collectionItemUrl == expected }
     }
 }
